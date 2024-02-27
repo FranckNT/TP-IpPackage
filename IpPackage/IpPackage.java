ac60f1dc3e617fb1f2edd6cd6044f2cd34953b10
FranckNT <156696744+FranckNT@users.noreply.github.com>
2024-02-27 12:27:57 +0100

    Create IpPackage.java

---
+++ IpPackage/IpPackage.java
@@ -0,0 +1,16 @@
+package IpPackage;
+public class IpPackage{
+
+
+  
+  // Constructeur 
+
+  private IpPackage (int var0, int var1, int var2, int var3) { 
+
+        System.out.printIn("o1 = " + var0);
+        System.out.printIn("o2 = " + var1);
+        System.out.printIn("o3 = " + var2);
+        System.out.printIn("o4 = " + var3);
+       
+
+/////
